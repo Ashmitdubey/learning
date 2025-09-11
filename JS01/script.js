c70336541a2bb80d943b13669703e92dc20e31b4
Ashmitdubey <dubeyashmit2606@gmail.com>
2025-09-11 16:08:37 +0530

    JSday

---
+++ JS01/script.js
@@ -0,0 +1,21 @@
+// alert("Hello World");
+
+console.log("code is running")
+console.log("code is aksing for trouble")
+console.log("code is looking good")
+ let x = "Ashmit Dubey"
+ let y = 22
+ let z = 3.55;
+ const p = true
+ let q = undefined;
+ let r = null;
+
+ console.log(x, y, z, p, q, r);
+ console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+ let o = {
+    name: "Ashmit",
+    age: 22,
+    isAdult: true
+ }
+    console.log(o);
