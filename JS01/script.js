396108074e6739b9fdbc6bfd8087c9c00005a974
Ashmitdubey <dubeyashmit2606@gmail.com>
2025-09-16 15:45:07 +0530

    js02

--- JS01/script.js
+++ JS01/script.js
@@ -1,21 +1,106 @@
 // alert("Hello World");
 
-console.log("code is running")
-console.log("code is aksing for trouble")
-console.log("code is looking good")
- let x = "Ashmit Dubey"
- let y = 22
- let z = 3.55;
- const p = true
- let q = undefined;
- let r = null;
+// console.log("code is running")
+// console.log("code is aksing for trouble")
+// console.log("code is looking good")
+//  let x = "Ashmit Dubey"
+//  let y = 22
+//  let z = 3.55;
+//  const p = true
+//  let q = undefined;
+//  let r = null;
 
- console.log(x, y, z, p, q, r);
- console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+//  console.log(x, y, z, p, q, r);
+//  console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
 
- let o = {
-    name: "Ashmit",
-    age: 22,
-    isAdult: true
- }
-    console.log(o);
+// //  let o = {
+//     name: "Ashmit",
+//     age: 22,
+//     isAdult: true
+//  }
+//     console.log(o);
+
+//  console.log("Hello I am conditional tutorial")
+
+// let age = 1;
+// let grace = 2;
+
+// age += grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+/*
+I am a 
+multiline 
+comment
+*/
+
+// if (age == 18) {
+//     console.log("You can drive");
+// }
+
+// else if (age == 0) {
+//     console.log("Are you kidding?")
+// }
+
+// else if (age == 1) {
+//     console.log("Are you again kidding?")
+// }
+
+// else {
+//     console.log("You cannot drive");
+// }
+
+
+// a = 6;
+// b = 8;
+// let c = a > b ? (a - b) : (b - a);
+
+// /*
+// translates to:
+// if(a>b){
+//     let c = a - b;
+// }
+// else {
+//     let c = a - b;
+// }
+
+// */   
+
+// // Loops JavaScript 
+
+// let a= 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+// for(let i=0; i<5; i++){
+//     console.log(a+i);
+
+// for (let x = 0; x < 10; x++) {
+//    console.log(a + x); 
+   
+
+
+// let obj = {
+//     name: "Ashmit",
+//     age: 22,
+//     company: "implitude"
+// }
+
+// for (const key in obj) {
+//    const element = obj[key];
+//    console.log(key, element);  
+// }
+
+let i=5;
+while(10>i){
+   i++;
+    console.log(i);
+   //  i--;
+}
