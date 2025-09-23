f4c3555ad9da97ef3e8a7f06a424a117c6ac14d8
Ashmitdubey <dubeyashmit2606@gmail.com>
2025-09-23 16:34:38 +0530

    22sept

--- JS01/script.js
+++ JS01/script.js
@@ -127,38 +127,45 @@ comment
 // }
 
 
-console.log("This is strings tutorial")
-let a = "Harry";
-console.log(a[0]);
-console.log(a[1]);
-console.log(a[2]);
-console.log(a[3]);
-console.log(a[4]);
-// console.log(a[5]);
+// console.log("This is strings tutorial")
+// let a = "Harry";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// // console.log(a[5]);
 
-console.log(a.length)
+// console.log(a.length)
 
-let real_name = "Harry"
-let friend = "Rohan"
-console.log("His name is " + real_name + " and his friends name is " + friend)
-console.log(`His name is ${real_name} and his friends name is ${friend}`)
+// let real_name = "Harry"
+// let friend = "Rohan"
+// console.log("His name is " + real_name + " and his friends name is " + friend)
+// console.log(`His name is ${real_name} and his friends name is ${friend}`)
 
-let b = "ShivamSh"
-console.log(b.toUpperCase())
-console.log(b.toLowerCase()) 
-console.log(b.length) 
-console.log(b.slice(1, 5)) 
-console.log(b.slice(1)) 
+// let b = "ShivamSh"
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase()) 
+// console.log(b.length) 
+// console.log(b.slice(1, 5)) 
+// console.log(b.slice(1)) 
 
-console.log(b.replace("Sh", "77"))
-console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+// console.log(b.replace("Sh", "77"))
+// console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
 
-console.log(b)
+// console.log(b)
 
-let m = 6
-function factorial(number) {
-    let arr = Array.from(Array(number).keys)
-    console.log(arr)
-}
+// let m = 6
+// function factorial(number) {
+//     let arr = Array.from(Array(number).keys)
+//     console.log(arr)
+// }
 
-factorial(a)
+// factorial(a)
+console.log("script.js initializing")
+let boxes = document.getElementsByClassName("box1")
+console.log(boxes)
+
+Array.from(boxes).forEach(e=>{
+    console.log(e)
+})
--- JS01/tempCodeRunnerFile.js
+++ JS01/tempCodeRunnerFile.js
@@ -1,5 +1,16 @@
 
-// let random = Math.random();
-// let a = prompt("Enter first number");
-// let b = prompt("Enter second number");
-// let c = prompt("Enter operation");
+// console.log("This is strings tutorial")
+// let a = "Harry";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// // console.log(a[5]);
+
+// console.log(a.length)
+
+// let real_name = "Harry"
+// let friend = "Rohan"
+// console.log("His name is " + real_name + " and his friends name is " + friend)
+// console.log(`His name is ${real_name} and his friends name is ${friend}`)
